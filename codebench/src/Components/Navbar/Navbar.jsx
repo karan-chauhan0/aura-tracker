4068f776af30fb471e6b1e10fb50b99e9f3265d2
AdhirajSinha <sinhaadhiraj.240@gmail.com>
2024-11-05 23:57:32 +0530

    navbar component

---
+++ codebench/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
+import './Navbar.css';
+import logo from './logo.png';
+
+const Navbar = () => {
+    const { loginWithRedirect, logout, isAuthenticated, user } = useAuth0();
+    const navigate = useNavigate(); 
+
+    return (
+        <header className="header">
+            <a href="/" className="logo">
+                <img src={logo} alt="Logo" />
+            </a>
+            <nav className="navbar">
+                <a href="#home">Home</a>
+                <a href="#about">About</a>
+                <a href="#courses">Courses</a>
+                <a href="#services">Services</a>
+                <a href="#contact">Contact</a>
+                <a href="/">
+                {isAuthenticated ? (
+                    <>
+                        <button onClick={() => logout({ returnTo: window.location.origin })}>Log Out</button>
+                    </>
+                ) : (
+                    <></>
+                )}
+                </a>
+                
+            </nav>
+        </header>
+    );
+};
+
+export default Navbar;
